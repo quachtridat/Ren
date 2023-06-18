7b411a238bcce22da117a53c51817a4d078745bb
xXJadeRabbitXx <75904609+xXJadeRabbitXx@users.noreply.github.com>
2023-06-17 22:50:50 -0700

    [VxTwit] React to message edits (#650)
    * adding edit support
    
    * restoring debug changes
    
    * mr fixes
    
    * revert embed update
    
    * moving to new helpers file
    
    ---------
    
    Co-authored-by: xXJadeRabbitXx <75904609+xXJadeRabbitXx@users.noreply.github.com>

--- cogs/vxtwitconverter/eventsCore.py
+++ cogs/vxtwitconverter/eventsCore.py
@@ -1,18 +1,13 @@
-from discord import Message, channel
-from urlextract import URLExtract
+from discord import Message
 
 from .constants import KEY_ENABLED
 from .core import Core
+from .helpers import convert_to_vx_twitter_url, urls_to_string, valid
 
 
 class EventsCore(Core):
-    async def _twit_replacer(self, message: Message):
-        # skips if the message is sent by any bot
-        if message.author.bot:
-            return
-
-        # skips if message is in dm
-        if isinstance(message.channel, channel.DMChannel):
+    async def _on_message_twit_replacer(self, message: Message):
+        if not valid(message):
             return
 
         if not await self.config.guild(message.guild).get_attr(KEY_ENABLED)():
@@ -21,29 +16,52 @@ class EventsCore(Core):
             )
             return
 
-        # skips if the message has no embeds
-        if not any(embed.video for embed in message.embeds):
-            return
-
         # the actual code part
-        vx_twit_links = [
-            result.replace("https://twitter.com", "https://vxtwitter.com")
-            for result in URLExtract().find_urls(message.content)
-            if "https://twitter.com" in result
-        ]
+        vx_twtter_urls = convert_to_vx_twitter_url(message.embeds)
 
-        # if we can't find any twitter links
-        if not vx_twit_links:
-            self.logger.debug("Embed found, but cannot find any valid links in the message")
+        # no changed urls detected
+        if not vx_twtter_urls:
             return
 
         # constructs the message and replies with a mention
-        ok = await message.reply(
-            "OwO what's this?\n"
-            "*notices your terrible twitter embeds*\n"
-            "Here's a better alternative:\n" + "\n".join(vx_twit_links),
-        )
+        ok = await message.reply(urls_to_string(vx_twtter_urls))
 
         # Remove embeds from user message if reply is successful
         if ok:
             await message.edit(suppress=True)
+
+    async def _on_edit_twit_replacer(self, message_before: Message, message_after: Message):
+        # skips if the message is sent by any bot
+        if not valid(message_after):
+            return
+
+        if not await self.config.guild(message_after.guild).get_attr(KEY_ENABLED)():
+            self.logger.debug(
+                "VxTwit disabled for guild %s (%s), skipping",
+                message_after.guild.name,
+                message_after.guild.id,
+            )
+            return
+
+        video_embed_before = [embed for embed in message_before.embeds if embed.video]
+        video_embed_after = [embed for embed in message_after.embeds if embed.video]
+        new_video_embeds = [
+            embed for embed in video_embed_after if embed not in video_embed_before
+        ]
+
+        # skips if the message has no new embeds
+        if not new_video_embeds:
+            return
+
+        vx_twtter_urls = convert_to_vx_twitter_url(new_video_embeds)
+
+        # no changed urls detected
+        if not vx_twtter_urls:
+            return
+
+        # constructs the message and replies with a mention
+        ok = await message_after.reply(urls_to_string(vx_twtter_urls))
+
+        # Remove embeds from user message if reply is successful
+        if ok:
+            await message_after.edit(suppress=True)
